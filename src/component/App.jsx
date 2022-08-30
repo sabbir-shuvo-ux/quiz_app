024cb18c3fdc084cfeda901705adb766bc2b3854
sabbir-shuvo-ux <79780278+sabbir-shuvo-ux@users.noreply.github.com>
2022-08-30 10:02:35 +0600

    Update App.jsx

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthProveider } from "../contexts/Authcontext";
 import "../styles/App.css";
 import Layout from "./Layout";
